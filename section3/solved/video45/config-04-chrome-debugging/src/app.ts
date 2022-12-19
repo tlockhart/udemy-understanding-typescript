6db90bd94e6f0c5f5c283bf01549cc4dc487e8d4
Tony Lockhart <tony.lockhart@pearson.com>
2022-12-19 13:06:55 -0500

    chore: Added detailed debugger example

--- section3/solved/video45/config-04-chrome-debugging/src/app.ts
+++ section3/solved/video45/config-04-chrome-debugging/src/app.ts
@@ -5,12 +5,20 @@ function add(n1: number, n2: number) {
   if (n1 + n2 > 0) {
     return n1 + n2;
   }
-  return;
+  return null;
+}
+function nestedFunction() {
+  const status = "Executing nestedFunction"
+  return status;
 }
 
-function clickHandler(message: string) {
-  // let userName = 'Max';
-  console.log('Clicked! ' + message);
+function clickHandler( message1: string) {
+  console.log(message1);
+  const inputValue = (<HTMLInputElement>document.getElementById("fname")).value;
+  const message2 = `inputValue: ${inputValue}`;
+  console.log(message2);
+  const message3 = nestedFunction();
+  console.log(message3);
 }
 // a comment
 if (button) {
@@ -24,5 +32,5 @@ if (button) {
    However, since there is no class, there is no need to bind that clickHandler to anything.  Therefore, you can bind the clickHander to null
   // Binding Explanation: https://www.youtube.com/watch?v=Pv9flm-80vM
   ***********************************************/
-  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+  button.addEventListener('click', clickHandler.bind(null, "Executing Click Handler"));
 }
