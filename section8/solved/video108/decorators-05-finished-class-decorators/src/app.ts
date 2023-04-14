6ea7939dea5864849febbf9f728a43e653bad95f
Tony Lockhart <tony.lockhart@pearson.com>
2023-04-14 02:25:53 -0400

    chore: Updated video108 sample code

--- section8/solved/video108/decorators-05-finished-class-decorators/src/app.ts
+++ section8/solved/video108/decorators-05-finished-class-decorators/src/app.ts
@@ -1,35 +1,89 @@
-function Logger(logString: string) {
-  console.log('LOGGER FACTORY');
-  return function(constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
+const employeesData = [
+  {
+    name: "Tony",
+  },
+  {
+    name: "Sammy",
+  },
+];
+function Controller(controllerGreeting: string, targetName: string, employeesData: { name: string }[]) {
+  console.log(controllerGreeting);
+  
+  // Constructor is passed from the Person Class:
+  return function (constructor: typeof Person) {
+    /*******************************
+     * Example 1: Original employeesData
+    ********************************/
+    const person = new constructor(targetName, employeesData);
+    console.log("Controller: Original Employees:", person.employees);
+    // person.updateName("Anthony", "Bobby");
+    // console.log("Person New Name1:", person, person.employees);
+    /***************************/
+    // Return a class with a constructor 
+    return class extends Person {
+      constructor(targetName: string, employeesData: { name: string }[], newName: string = "") {
+        super(targetName, employeesData, newName,);
+      }
+    };
   };
 }
 
-function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY');
-  return function(constructor: any) {
-    console.log('Rendering template');
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
-    }
-  }
+function NameUpdater(nameUpdaterGreeting: string, targetName: string, newName: string, employeesData: { name: string }[]) {
+  console.log(nameUpdaterGreeting);
+  
+  // The constructor is auto passed in from the Controller return stmt
+  return function (constructor: typeof Person) {
+    /**
+     * Override updateName method in person 
+     * constructor passed from Controller
+     * Note: updateName method must have the  
+     * same signature as the Person class.
+     */
+    constructor.prototype.updateName = function (_targetName: string, _newName: string) {
+      // console.log("Employees:", employeesData);
+      const employeeMatch = employeesData.find(
+        (employee: { name: string }) => employee.name === targetName
+      );
+      if (employeeMatch) {
+        employeeMatch.name = newName;
+      } else {
+        console.log(`Could not find employee with name '${targetName}'`);
+      }
+    }; // constructor overide
+
+    /*************************************
+     * Example 2: Updated employeesData
+     * ************************************/
+    const person = new constructor(targetName, employeesData, newName);
+    person.updateName(targetName, newName);
+    console.log("NameUpdater: Update Employees:", person.employees)
+  };
 }
 
-// Example 1: Multiple Decorators: 
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
+// @WithTemplate('<h1>My Person Object</h1>', 'app')
+
+// Multiple Decorators Class: 
+@Controller("Hello from Controller:", "Tony", employeesData)
+@NameUpdater("Hello from NameUpdater:", "Tony", "Anthony", employeesData)
 class Person {
-  name = 'Max';
+  targetName = "";
+  employees: { name: string }[] = employeesData;
+  constructor(targetName: string, _employeesData: { name: string }[], _newName = "") {
+    this.targetName = targetName;
+    console.log("Person Constructor: Creating person object...");
+  }
 
-  constructor() {
-    console.log('Creating person object...');
+  updateName(updateName: string, targetName: string) {
+    const employeeMatch = this.employees.find(
+      (employee) => employee?.name === targetName
+    )!;
+    employeeMatch.name = updateName;
+    console.log("Person New Name:", this.employees)
   }
 }
-
-const pers = new Person();
-
-console.log(pers);
+/***************************************
+ * Example3: Decorator Execution is completely 
+ * seperate from class instantiation
+ ****************************************/
+// const pers1 = new Person("Tony", employeesData);
+// console.log("Pers1:", pers1); 
