4adeb8cd0f86ce357fce22a1fa1b802331c8f5b6
Tony Lockhart <tony.lockhart@pearson.com>
2023-04-16 00:36:05 -0400

    feat: Added video114 code samples

--- section8/solved/video114/decorators-09-example-autobind/src/app.ts
+++ section8/solved/video114/decorators-09-example-autobind/src/app.ts
@@ -1,129 +1,54 @@
-function Logger(logString: string) {
-  console.log('LOGGER FACTORY');
-  return function(constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
-}
-
-function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY');
-  return function<T extends { new (...args: any[]): {name: string} }>(
-    originalConstructor: T
-  ) {
-    return class extends originalConstructor {
-      constructor(..._: any[]) {
-        super();
-        console.log('Rendering template');
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-          hookEl.innerHTML = template;
-          hookEl.querySelector('h1')!.textContent = this.name;
-        }
-      }
-    };
-  };
-}
-
-// @Logger('LOGGING - PERSON')
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
-class Person {
-  name = 'Max';
-
-  constructor() {
-    console.log('Creating person object...');
-  }
-}
-
-const pers = new Person();
-
-console.log(pers);
-
-// ---
-
-function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator!');
-  console.log(target, propertyName);
-}
-
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log('Accessor decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log3(
-  target: any,
-  name: string | Symbol,
-  descriptor: PropertyDescriptor
-) {
-  console.log('Method decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log4(target: any, name: string | Symbol, position: number) {
-  console.log('Parameter decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
-
-class Product {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error('Invalid price - should be positive!');
-    }
-  }
-
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
-
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this._price * (1 + tax);
-  }
-}
-
-const p1 = new Product('Book', 19);
-const p2 = new Product('Book 2', 29);
-
-function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+/**
+ * Exampld 2: Build a decorator that will autobind the this 
+ * in the showMessage printer method to the printer object
+ * it belongs to
+ */
+// Syntax: Autobind(target: any, methodName: string | Symbol | number, descriptor: PropertyDescriptor)
+function Autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+  // Step1: Get a reference to the showMessage method from the propertyDescriptor.value property
   const originalMethod = descriptor.value;
+
+  // Step2: Setup another propertyDescriptor to be returned
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
     enumerable: false,
+    // Step3: Add a getter to setup the new logic before we execute the function
     get() {
+      // Step4: Bind this ( the printer object is what the this refers to)
       const boundFn = originalMethod.bind(this);
       return boundFn;
     }
   };
+  // Step5: Return the new propertyDescriptor to override the old propertyDescriptor
   return adjDescriptor;
 }
 
 class Printer {
   message = 'This works!';
 
+  // Add AutoBind to the showMessage method to bind the this 
   @Autobind
   showMessage() {
-    console.log(this.message);
+    /**
+     * When calling the showMessage in a 
+     * Printer instance, we must bind the 
+     * this, so this refers to the class instead
+     */
+    alert(this.message);
   }
 }
 
 const p = new Printer();
-p.showMessage();
-
 const button = document.querySelector('button')!;
+/**
+ * Example1: trigger show message on click,
+ * bind p to the Printer class, instead of the 
+ * button using bind property
+ */
+// button.addEventListener('click', p.showMessage.bind(p));
+/**
+ * Example2: trigger show message on click,
+ * bind p to the Printer class, instead of the 
+ * button using AutoBind Decorator
+ */
 button.addEventListener('click', p.showMessage);
