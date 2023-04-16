7c1583829a27df41238404ecfe59ea516fd74c7a
Tony Lockhart <tony.lockhart@pearson.com>
2023-04-16 14:43:47 -0400

    feat: Added video116 sample code

---
+++ section8/solved/video115/decorators-09-example-autobind/src/app.ts
@@ -0,0 +1,37 @@
+function Required() {}
+
+function PositiveNumber(){}
+
+function validate(_obj: object){}
+
+class Course {
+  // @Required
+  title: string;
+  // @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+  //Example1: Create a course instance from input data
+  const courseForm = document.querySelector("form")!;
+  courseForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const titleEl = document.getElementById("title") as HTMLInputElement;
+    const priceEl = document.getElementById("price") as HTMLInputElement;
+
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+    const createdCourse = new Course(title, price);
+    // if(!validate(createdCourse)){
+      alert("Invalid input, please try again.")
+      return;
+    // }
+    console.log(createdCourse);
+  })
+
+  // Example2: Validate Form Input Data with eventListener
--- section8/solved/video116/decorators-10-decorator-validation/src/app.ts
+++ section8/solved/video116/decorators-10-decorator-validation/src/app.ts
@@ -1,179 +1,102 @@
-function Logger(logString: string) {
-  console.log('LOGGER FACTORY');
-  return function(constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
-}
-
-function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY');
-  return function<T extends { new (...args: any[]): { name: string } }>(
-    originalConstructor: T
-  ) {
-    return class extends originalConstructor {
-      constructor(..._: any[]) {
-        super();
-        console.log('Rendering template');
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-          hookEl.innerHTML = template;
-          hookEl.querySelector('h1')!.textContent = this.name;
-        }
-      }
-    };
-  };
-}
-
-// @Logger('LOGGING - PERSON')
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
-class Person {
-  name = 'Max';
-
-  constructor() {
-    console.log('Creating person object...');
-  }
-}
-
-const pers = new Person();
-
-console.log(pers);
-
-// ---
-
-function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator!');
-  console.log(target, propertyName);
-}
-
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log('Accessor decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log3(
-  target: any,
-  name: string | Symbol,
-  descriptor: PropertyDescriptor
-) {
-  console.log('Method decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log4(target: any, name: string | Symbol, position: number) {
-  console.log('Parameter decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
-
-class Product {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error('Invalid price - should be positive!');
-    }
-  }
-
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
-
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this._price * (1 + tax);
-  }
-}
-
-const p1 = new Product('Book', 19);
-const p2 = new Product('Book 2', 29);
-
-function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
-  const adjDescriptor: PropertyDescriptor = {
-    configurable: true,
-    enumerable: false,
-    get() {
-      const boundFn = originalMethod.bind(this);
-      return boundFn;
-    }
-  };
-  return adjDescriptor;
-}
-
-class Printer {
-  message = 'This works!';
-
-  @Autobind
-  showMessage() {
-    console.log(this.message);
-  }
-}
-
-const p = new Printer();
-p.showMessage();
-
-const button = document.querySelector('button')!;
-button.addEventListener('click', p.showMessage);
-
-// ---
-
+/**
+ * Register class properties (title, price) and their validators 
+ * in ValidatorConfig when the Course class is defined
+ */
+//validater blueprint
+// Validator Type Syntax: Course : { price: ["required"], title:["positive"]}
 interface ValidatorConfig {
   [property: string]: {
-    [validatableProp: string]: string[]; // ['required', 'positive']
-  };
+    [validatableProp: string]: string[]
+  }
 }
-
+ 
+/**
+ * Important: Initially registeredValidators object is empty, but will be loaded
+ *  by Postive and Required decorator on Course class declaration
+ */
+// Syntax: {validatorsConfig,}
 const registeredValidators: ValidatorConfig = {};
 
-function Required(target: any, propName: string) {
-  registeredValidators[target.constructor.name] = {
-    ...registeredValidators[target.constructor.name],
-    [propName]: ['required']
+//Import: Adds a validator to the registeredValidators object
+function Required(classInstTarget: any, propName: string) {
+  /**
+   * Register the class Name (Course) in
+   * registeredValidators object, by the name prop
+   * Note: class instance passed in as prop
+   */
+  registeredValidators[classInstTarget.constructor.name] = {
+     // Important: Make sure you retain all existing validators and not overwrite
+     ...registeredValidators[classInstTarget.constructor.name],
+    [propName]: [...(registeredValidators[classInstTarget.constructor.name]?.[propName] ?? []),"required"]
   };
 }
 
-function PositiveNumber(target: any, propName: string) {
-  registeredValidators[target.constructor.name] = {
-    ...registeredValidators[target.constructor.name],
-    [propName]: ['positive']
+function PositiveNumber(classInstTarget: any, propName: string) {
+  // Register the className in registeredValidators
+  registeredValidators[classInstTarget.constructor.name] = {
+    // Important: Make sure you retain all existing validators and not overwrite
+    ...registeredValidators[classInstTarget.constructor.name],
+    [propName]: [...(registeredValidators[classInstTarget.constructor.name]?.[propName] ?? []),"positive"]
   };
 }
 
+/**
+ * Should loop through all registeredValidators,
+ * And validator the title and price properties
+ */
 function validate(obj: any) {
+  console.log("ObjectInstance:", obj);
+  /**
+   * Step3: To retrieve the configuration for the object we are dealling with
+   * first determine the name of the class (Course) this object is instantiated
+   *  from. Get the object's name (Course) from the constructor property.
+   * Then get the validatorConfig we set up for that object, by using the
+   * name as the key.
+   * IMPORTANT: All Registers have been added Initally by the Required
+   * Decorator
+   */
   const objValidatorConfig = registeredValidators[obj.constructor.name];
+  console.log("RegisteredValidator:", registeredValidators);
+  console.log("Object Name:", obj.constructor.name);
+  console.log("Course Object on RegisteredValidator:", objValidatorConfig);
+  // Step4: if there is no objValidator Config the object is valid
   if (!objValidatorConfig) {
     return true;
-  }
+  } 
+  /**
+   * Make sure we check both title and price validator
+   * properties, by setting isValid to false if one is not valid
+   */
   let isValid = true;
-  for (const prop in objValidatorConfig) {
-    for (const validator of objValidatorConfig[prop]) {
-      switch (validator) {
-        case 'required':
-          isValid = isValid && !!obj[prop];
-          break;
-        case 'positive':
-          isValid = isValid && obj[prop] > 0;
-          break;
+
+    // console.log("ObjValidatorConfig:", objValidatorConfig);
+    // If we do find the class name, loop through the registeredValidator keys
+    for (const validatorProp in objValidatorConfig) {
+      console.log("ValidatorProp", validatorProp);
+      // Validate the title and price properties for each validator key (Course)
+      for (const validator of objValidatorConfig[validatorProp]) {
+        switch (validator) {
+          case "required":
+            // if the title prop has a none empty update isValid
+            isValid = isValid && !!obj[validatorProp]; // make a boolean
+            break;
+          case "positive":
+            // if the price prop is > 0 update isValid
+            isValid = isValid && obj[validatorProp] > 0;
+            break;
+        }
       }
     }
-  }
-  return isValid;
+    // Step5: If we complete the for loop all properties are valid
+    console.log("Object is valid:", isValid)
+    return isValid;
 }
 
 class Course {
+  /**
+   * Step0: All instance variables  are loaded to registeredValidator 
+   * at class declaration
+   */
   @Required
   title: string;
   @PositiveNumber
@@ -185,20 +108,25 @@ class Course {
   }
 }
 
-const courseForm = document.querySelector('form')!;
-courseForm.addEventListener('submit', event => {
+/**
+ * Example1: Create a course instance from input data 
+ * Register the instance variables (title, price) in a registeredValidators
+ * object and validate the data with a validator function
+ */
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", event => {
   event.preventDefault();
-  const titleEl = document.getElementById('title') as HTMLInputElement;
-  const priceEl = document.getElementById('price') as HTMLInputElement;
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
 
   const title = titleEl.value;
   const price = +priceEl.value;
-
+// Step1: Create course instance from input
   const createdCourse = new Course(title, price);
-
+  // Step2: Pass course instance to the validate function
   if (!validate(createdCourse)) {
-    alert('Invalid input, please try again!');
+    alert("Invalid input, please try again.")
     return;
   }
   console.log(createdCourse);
-});
+})
