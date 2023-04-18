9e1f6667821e4933feeb4d199dad41b7c3a58db5
Tony Lockhart <tony.lockhart@pearson.com>
2023-04-18 10:41:01 -0400

    chore: updated video108-NestedFunc sample code

--- section8/solved/video108-NestedFunc_FormValidation/decorators-05-finished-class-decorators/src/app.ts
+++ section8/solved/video108-NestedFunc_FormValidation/decorators-05-finished-class-decorators/src/app.ts
@@ -4,30 +4,33 @@
 /***************************
  * Calling an Object from a Class
  **************************/
-// type PersonConstructType = new(...args: any[])=>{targetName: string, employees: typeof employeesData, updateName: (updateName: string)=>{}};
+// type PersonConstructType = new (...args: any[]) => { targetName: string, employees: typeof employeesData, updateName: (updateName: string) => {} };
 
- const employeesData = [
-    {
-      name: "Tony",
-    },
-    {
-      name: "Sammy",
-    },
-  ];
-
+const employeesData = [
+  {
+    name: "Tony",
+  },
+  {
+    name: "Sammy",
+  },
+];
+/**
+ * Person: The purpose of this class is to search for
+ *  a target name and update it with a new name.
+ */
 class Person {
   targetName = "";
   employees = employeesData;
-  constructor(targetName: string,  _employeeData: typeof employeesData, _newName = "") {
+  constructor(targetName: string, _employeeData: typeof employeesData, _newName = "") {
     this.targetName = targetName;
-    console.log("Creating person object...");
+    console.log("Creating a new Person instance...");
   }
-
+  // Perform name search and update name to new Name
   updateName(updateName: string) {
     const employeeMatch = this.employees.find(
       (employee) => employee.name === this.targetName
     );
-    if(employeeMatch){
+    if (employeeMatch) {
       employeeMatch.name = updateName;
     }
   }
@@ -37,40 +40,63 @@ class Person {
  * (Maps to WithTemplate decorator) NameUpdaterDecorator function that receives Person Constructor, greeting,
  * and targetName from ControllerDecorator and calls the Person class
  */
-// function NameUpdaterDecorator(personConstructor: PersonConstructType, nameUpdaterGreeting: string, targetName: string, _newName: string) {
+// function NameUpdaterDecorator(
+//   personConstructor: PersonConstructType,
+//   nameUpdaterGreeting: string,
+//   targetName: string,
+//   _newName: string
+// ) {
+//   //Step3: Log the nameUpdaterGreeting
 //   console.log(nameUpdaterGreeting);
-//   // Instantiate the person instance
+//   // Step4: Instantiate the person instance
 //   const person = new personConstructor(targetName);
- 
-//   // Note: ShallowCopy will not work for objects with methods and nested props:
+
+//   // Example4: ShallowCopy - Only copies top-level obj. properties, 
+//   // but any nested objects or ARRAYS are still referenced by 
+//   // the original object, not copied.
+//   // **REMEMEBER TO COMMENT THE DEEPCLONE IMPORT
 //   // const personCopy = {...person};
 
-//   // Note: Must use a deep copy instead;
+//   // Step4: Clone original person with deep copy (building a new house with different address):
 //   const personCopy = deepCopy(person);
-//   console.log("Person2-OriginalEmployees:", personCopy.employees);
+
+//   // Step5: Log the original employee data
+//   console.log("NameUpdater: Person2-OriginalEmployees:", personCopy.employees);
+//   // Step6: Update the targetName with the newName
 //   person.updateName(_newName);
-//   console.log("Person2-NewEmployees:", person.employees);
-//   return person;
+//   // Step7: Log the new employee data
+//   console.log("NameUpdater:Person2-NewEmployees:", person.employees);
+//   // Step8: Return to ControllerDecorator
+//   return;
 // }
 
 /**
- * (Maps to Logger decorator) ControllerDecorator function that passes Person Constructer, greeting,
- * and targetName to NameUpdaterDecorator function
+ * (Maps to Logger decorator) ControllerDecorator function
+ * receives Person Constructer, greeting, and targetName, which 
+ * is passed as argements to the NameUpdaterDecorator function
  */
-// function ControllerDecorator(personConstructor: PersonConstructType, controllerGreeting: string, targetName: string, newName: string) {
+// function ControllerDecorator(
+//   personConstructor: PersonConstructType,
+//   controllerGreeting: string,
+//   targetName: string,
+//   newName: string
+// ) {
+//   // Step1: Output controllerGreeting
 //   console.log(controllerGreeting);
 
-//   // Return call to the NameUpdaterDecorator
-//   return NameUpdaterDecorator(
-//     personConstructor,
-//     "Hello from the NameUpdaterDecorator",
-//     targetName, 
-//     newName
-//   );
+//   // Step2: Call to the NameUpdaterDecorator
+//     NameUpdaterDecorator(
+//       personConstructor,
+//       "Hello from the NameUpdaterDecorator",
+//       targetName,
+//       newName
+//     );
+//     // Step9: Return string 
+//    return "ControllerDecorator: Finished Execution"; 
 // }
 
-//Example 1: Create a regular Person instance by instantiating a class
-//Note: Decorators run at class instantiation
+// //Example 1: Create a regular Person instance by instantiating a class
+// //Note: Decorators run at class instantiation
 const personInstance1 = new Person("Tony", employeesData);
 console.log("Person1-OriginalEmployee:", personInstance1.employees);
 
@@ -78,5 +104,7 @@ console.log("Person1-OriginalEmployee:", personInstance1.employees);
 // personInstance1.updateName("Anthony");
 // console.log("Person1-UpdatedEmployees:", personInstance1.employees);
 
-// // Example3: Create a person instance withIn the ControllerDecorator that can be manipulated
-// ControllerDecorator(Person as PersonConstructType, "Hello from the ControllerDecorator", "Tony", "Anthony");
+// Example3: Create a person instance withIn the ControllerDecorator that can be manipulated
+//** REMEMBER uncomment DEEPCOPY Import and PersonConstructType */
+// Step10: Log Conroller Decorator Finished:
+// console.log(ControllerDecorator(Person as PersonConstructType, "Hello from the ControllerDecorator", "Tony", "Anthony"));
